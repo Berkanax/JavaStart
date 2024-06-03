7fa17dce33ffd4a17d9f3bcdf419d141e3b63586
Adrian <adrian.mosk@outlook.com>
2024-06-03 21:34:42 +0200

    Update 1.0

--- src/main/java/org/example/Task05.java
+++ src/main/java/org/example/Task05.java
@@ -1,4 +1,25 @@
 package org.example;
+/*
+Napisz program, w którym zadeklarujesz zmienne, które będą reprezentować samochód w salonie samochodowym poprzez:
 
+Markę
+Model
+Rocznik
+Cenę
+Możesz również dodać dowolne zmienne, które samodzielnie uznasz za stosowne. Do reprezentacji każdego z powyższych elementów wykorzystaj najbardziej odpowiedni Twoim zdaniem typ danych.
+
+Na końcu wyświetl informacje o samochodzie, które przypisałeś do zmiennych.
+ */
 public class Task05 {
+    public static void main(String[] args) {
+        System.out.println("Witaj w salonie JavaStart!");
+        Task05_01 car1 = new Task05_01("Seat", "Arona", 2021, 117_000);
+        Task05_01 car2 = new Task05_01("Toyota", "Corolla", 2020, 99_000);
+        Task05_01 car3 = new Task05_01("Ford", "Mustang", 2019, 180_000);
+
+        System.out.println(car1);
+        System.out.println(car2);
+        System.out.println(car3);
+
+    }
 }
---
+++ src/main/java/org/example/Task05_01.java
@@ -0,0 +1,20 @@
+package org.example;
+
+public class Task05_01 {
+    String brand;
+    String model;
+    int yearbook;
+    double price;
+
+    public Task05_01(String brand, String model, int yearbook, double price) {
+        this.brand = brand;
+        this.model = model;
+        this.yearbook = yearbook;
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Brand: " + brand + ", Model: " + model + ", Yearbook: " + yearbook + ", Price: " + price;
+    }
+}
